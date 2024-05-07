5f5d00b8403bd5f25b0bf3b4abca6d6acc57341f
Guilherme Moitinho <guilhermemoitinho3165@gmail.com>
2024-05-07 18:21:12 -0300

    add entity address

---
+++ src/models/Address.js
@@ -0,0 +1,14 @@
+const {Model, DataTypes} = require('sequelize');
+
+class Address extends Model{
+    static init(sequelize) {
+        super.init({
+            street: DataTypes.STRING,
+            number: DataTypes.STRING,
+            district: DataTypes.STRING,
+            city: DataTypes.STRING,
+        }, {
+            sequelize
+        })
+    }   
+}
