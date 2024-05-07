f11b19a9f5d911e34ec7c4cca542206390491652
Guilherme Moitinho <guilhermemoitinho3165@gmail.com>
2024-05-07 18:30:17 -0300

    add address and courses in routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,29 @@
 const express = require('express')
-const services = require('./services/UserService');
+const Userservices = require('./services/UserService');
+const AddressService = require('./services/AddressService');
+const CourseService = require('./services/CourseService');
 
 const authMiddleware = require('./middlewares/auth');
 
 const router = express.Router();
 
-router.get('/users', authMiddleware, services.index)
-router.get('/users/:id', services.userById)
-router.post('/users', services.store)
-router.put('/users/:id', services.update)
-router.delete('/users/:id', services.delete)
+router.get('/users', authMiddleware, Userservices.index)
+router.get('/users/:id', Userservices.userById)
+router.post('/users', Userservices.store)
+router.put('/users/:id', Userservices.update)
+router.delete('/users/:id', Userservices.delete)
 
-router.post('/users/login', services.login)
+router.post('/users/login', Userservices.login)
 
 router.use(authMiddleware);
+
+router.get('/users/:user_id/address', AddressService.index);
+router.post('/users/:user_id/address', AddressService.store);
+router.delete('/users/:id/address', AddressService.delete);
+router.put('/users/:id/address', AddressService.update);
+
+router.get('/users/:user_id/courses', CourseService.index);
+router.post('/users/:user_id/courses', CourseService.store);
+router.delete('/users/:user_id/courses', CourseService.delete);
 
 module.exports = router;
