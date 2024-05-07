032c9e3df44f3442eb5513f33e8f1cfb60f4600a
Guilherme Moitinho <guilhermemoitinho3165@gmail.com>
2024-05-07 13:28:55 -0300

    add Userservice

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const services = require('./services/UserService');
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    return res.json('teste')
+})
 
 router.get('/', (req, res) => {
     return res.json('teste')
